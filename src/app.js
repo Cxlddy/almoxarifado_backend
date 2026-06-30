cacab441b8da87d018f29fe8176893a0cba02e82
Cxlddy <rcds.ciampi@gmail.com>
2026-06-30 10:53:46 -0300

    bugs correction

--- src/app.js
+++ src/app.js
@@ -15,18 +15,14 @@ import authRoutes from './routes/auth.routes.js';
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:5000',
-
-    'https://cxlddy.github.io',
-    'https://almoxarifado-theta.vercel.app/'
-  ],
-  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.options('*', cors());
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ mensagem: 'API do almoxarifado funcionando' });
